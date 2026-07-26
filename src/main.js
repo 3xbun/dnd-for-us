b9c13c36ceda7b6d038feeffe01c3405702117e3
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2026-07-26 11:28:16 +0700

    Using umami to track

--- src/main.js
+++ src/main.js
@@ -1,17 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import VueMatomo from "vue-matomo";
 
 import router from "./routes/";
 
-createApp(App)
-  .use(router)
-  .use(VueMatomo, {
-    // Configure your matomo server and site by providing
-    host: "https://analyze.dnd-for.us",
-    siteId: 1,
-  })
-  .mount("#app");
-
-window._paq.push(["trackPageView"]);
+createApp(App).use(router).mount("#app");
