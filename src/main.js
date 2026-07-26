f44bec9468390cf27a2956b0b12270b14f5e5cb4
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2026-07-26 11:19:27 +0700

    Using vue matomo

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import VueMatomo from "vue-matomo";
 
 import router from "./routes/";
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .use(router)
+  .use(VueMatomo, {
+    // Configure your matomo server and site by providing
+    host: "https://analyze.dnd-for.us",
+    siteId: 1,
+  })
+  .mount("#app");
+
+window._paq.push(["trackPageView"]);
